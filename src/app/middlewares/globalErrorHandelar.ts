ff54b55613543f72cf9aa56c1ae108b726afd30c
Ad Kaium <adkaiumalikhan01@gmail.com>
2024-12-28 01:33:36 +0300

    zod Error Handeling

--- src/app/middlewares/globalErrorHandelar.ts
+++ src/app/middlewares/globalErrorHandelar.ts
@@ -1,12 +1,33 @@
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
-import  {  NextFunction, Request, Response } from 'express';
-
-export const globalErrorHandelar=(err: any, req: Request, res: Response, next: NextFunction) => {
+export const globalErrorHandelar: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
+) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'something is wrong!';
+  type TErrorSources = {
+    path: string | number;
+    message: string;
+  }[];
+  const errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Somethig is Wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    statusCode: 400;
+    message: 'This is zod Error';
+  }
   res.status(statusCode).json({
     seccess: false,
     message,
+    errorSources,
     error: err,
   });
 };
