ab7572d52e4225ed4b23e86b9c20b12f4701dc42
Ad Kaium <adkaiumalikhan01@gmail.com>
2024-12-27 03:15:34 +0300

    transection roll

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,4 +1,3 @@
-
 import config from '../../config';
 import { TStudent } from '../student/student.Interface';
 import { TUser } from './user.interface';
@@ -7,6 +6,8 @@ import { Student } from '../student/student.modle';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TAcademicSemester } from '../academicSemester/academicSemester.interface';
 import { generatestudentId } from './user.utils';
+import mongoose from 'mongoose';
+import AppError from '../../errors/appErro';
 
 //creat student
 const createNewStudent = async (password: string, payload: TStudent) => {
@@ -16,21 +17,38 @@ const createNewStudent = async (password: string, payload: TStudent) => {
   //    set password in created new obj.
   userData.password = password || (config.default_pass as string);
   userData.role = 'student';
-  
-  const admissionSemester = await AcademicSemester.findById(payload.admissionSemester) as TAcademicSemester;
 
-  userData.id = await generatestudentId(admissionSemester)
-  
-  const newUser = await User.create(userData);
+  const admissionSemester = (await AcademicSemester.findById(
+    payload.admissionSemester,
+  )) as TAcademicSemester;
 
-  //   create a student
-  if (Object.keys(newUser).length) {
-    (payload.id = newUser.id), (payload.user = newUser._id);
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+    userData.id = await generatestudentId(admissionSemester);
 
-    const newStudent = await Student.create(payload);
+    const newUser = await User.create([userData], { session });
+
+    //   create a student
+    if (!newUser.length) {
+      throw new AppError(401, 'User Create Faild');
+    }
+    (payload.id = newUser[0].id), (payload.user = newUser[0]._id);
+
+    const newStudent = await Student.create([payload], { session });
+    if (!newStudent) {
+      throw new AppError(401, 'Student Create Faild');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
     return newStudent;
+  } catch (err) {
+    await session.abortTransaction();
+    await session.endSession();
   }
 };
+
 
 export const userService = {
   createNewStudent,
