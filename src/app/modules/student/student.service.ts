bde3cdb03efedd70ac1429f941b89f0bed5c134f
Ad Kaium <adkaiumalikhan01@gmail.com>
2025-01-03 00:04:16 +0300

    create query builder class

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -3,76 +3,31 @@ import { Student } from './student.modle';
 import AppError from '../../errors/appErro';
 import { User } from '../user/user.model';
 import { TStudent } from './student.Interface';
+import QueryBuilder from '../../builder/QueryBuilder';
+import { studentSearchableFields } from './student.constent';
 
 // get all student
 const getAllStudent = async (query: Record<string, unknown>) => {
-  const queryObj = { ...query };
+  const studentQuery = new QueryBuilder(
+    Student.find()
+      .populate('admissionSemester')
+      .populate({
+        path: 'academicDepartment',
+        populate: {
+          path: 'academicFaculty',
+        },
+      }),
+    query,
+  ).search(studentSearchableFields)
+  .filter()
+  .sort()
+  .paginate()
+  .fields();
 
-  const searchableFields = ['email', 'name.firstName', 'presentAddress'];
-  let searchTerm = '';
-  if (query?.searchTerm) {
-    searchTerm = query?.searchTerm as string;
-  }
-  const studentSearchQuery = Student.find({
-    $or: searchableFields.map((filed) => ({
-      [filed]: { $regex: searchTerm, $options: 'i' },
-    })),
-  });
 
-  const excludeFields = ['searchTerm', 'sort','limit','page','fields'];
-  excludeFields.forEach((el) => delete queryObj[el]);
-
-  const filterQuery = studentSearchQuery
-    .find(queryObj)
-    .populate('admissionSemester')
-    .populate({
-      path: 'academicDepartment',
-      populate: {
-        path: 'academicFaculty',
-      },
-    });
-
-  let sort = '-createdAt';
-
-  if (query.sort) {
-    sort = query.sort as string;
-  }
-
-  const sortQuery = filterQuery
-    .sort(sort)
-    .populate('admissionSemester')
-    .populate({
-      path: 'academicDepartment',
-      populate: {
-        path: 'academicFaculty',
-      },
-    });
+  const result = await studentQuery.modelQuery;
+  return result;
   
-  let limit = 1;
-  let page = 1;
-  let skip = 0;
-   if (query.limit) {
-     limit = Number(query.limit);
-   }
-  if (query.page) {
-    page = Number(query.page);
-    skip = (page - 1) * limit;
-  }
-   
-  const paginateQuery = sortQuery.skip(skip);
-
-  const limitQuery =  paginateQuery.limit(limit);
-
-  let fields = '__v';
-
-  if(query.fields){
-    fields =(query.fields as string).split(',').join(' ');
-
-  }
-
-  const selectQuery = await limitQuery.select(fields);
-
-  return selectQuery;
 };
 
 // get one student by Id:
