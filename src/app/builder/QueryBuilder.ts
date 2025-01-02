7e64416a6d2601679499682f2609b10f46541508
Ad Kaium <adkaiumalikhan01@gmail.com>
2025-01-03 00:38:45 +0300

    create faculty

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -11,7 +11,6 @@ class QueryBuilder<T> {
 
   search(searchableFields: string[]) {
     const searchTerm = this?.query?.searchTerm;
-
     if (searchTerm) {
       this.modelQuery = this.modelQuery.find({
         $or: searchableFields.map(
@@ -22,49 +21,48 @@ class QueryBuilder<T> {
         ),
       });
     }
+
     return this;
   }
 
-   filter(){
-    const queryObj = {...this.query};
+  filter() {
+    const queryObj = { ...this.query }; // copy
 
-    // filltering
-    const excludeFields = ['serachTerm', 'page', 'limit', 'sort','fields'];
-    excludeFields.forEach(el => delete queryObj[el]);
+    // Filtering
+    const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
+
+    excludeFields.forEach((el) => delete queryObj[el]);
+
     this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
 
     return this;
-   }
+  }
 
-    sort(){
-        const sort = (this?.query?.sort as string).split(',').join(' ') || '-createdAt';
+  sort() {
+    const sort =
+      (this?.query?.sort as string)?.split(',')?.join(' ') || '-createdAt';
+    this.modelQuery = this.modelQuery.sort(sort as string);
 
-        this.modelQuery = this.modelQuery.sort(sort as string);
+    return this;
+  }
 
-        return this;
-    }
+  paginate() {
+    const page = Number(this?.query?.page) || 1;
+    const limit = Number(this?.query?.limit) || 10;
+    const skip = (page - 1) * limit;
 
-    paginate(){
-        const page = Number(this?.query?.page) || 1;
-        const limit = Number(this?.query?.limit) || 10;
-        const skip = (page - 1) * limit;
+    this.modelQuery = this.modelQuery.skip(skip).limit(limit);
 
-          this.modelQuery = this.modelQuery.skip(skip).limit(limit);
+    return this;
+  }
 
-        return this;
-    }
+  fields() {
+    const fields =
+      (this?.query?.fields as string)?.split(',')?.join(' ') || '-__v';
 
-    fields(){
-        const fields = (this?.query?.fields as string)?.split(',')?.join('') || '__v';
-
-        this.modelQuery = this.modelQuery.select(fields);
-
-        return this;
-    }
-    
-
-
+    this.modelQuery = this.modelQuery.select(fields);
+    return this;
+  }
 }
 
 export default QueryBuilder;
-
